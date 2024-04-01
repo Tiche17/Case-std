b3edc02fb5edea35c31a8bfcd0e5d032619b04df
Tiche12 <stshimanga1@ivytech.edu>
2024-04-01 19:08:53 -0400

    Modcasstd

---
+++ main.py
@@ -0,0 +1,12 @@
+last_name = input('Enter a last name (or ZZZ to exit): ') 
+while last_name != 'ZZZ' : 
+    first_name = input('Enter a first name: ')
+    gpa = float (input ('Enter a gpa: ') )
+    if gpa >= 3.5: 
+        print ('The student has made the Dean list')
+    elif gpa >= 3.25:
+        print ('The student has made the Honor roll') 
+    else :
+        print ('The student did not make the Dean list or the Honor roll')
+    last_name = input('Enter a name (or ZZZ to exit): ') 
+  
